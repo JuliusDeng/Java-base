56a1691ac15078acebb158b285cc769d1a661fb5
JuliusDeng <1576554007@qq.com>
2020-12-13 16:07:20 +0800

    Java基础。（java 从入门到精通！）

---
+++ Contacts/src/com/atguigu/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.com;
+
+/**
+ * @author LENOVO
+ *
+ */
+public class HelloWorld {
+	
+	 public static void main(String[] args) {
+		 
+		 System.out.println("hello,java22!");
+	 }
+}
